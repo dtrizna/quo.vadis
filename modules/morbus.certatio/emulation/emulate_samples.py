a1ddbca134f5abf0c819bed05355f8f0032b581a
Dmitrijs Trizna <dmitrijs.trizna@avast.com>
2022-02-17 12:28:34 +0100

    emulation collection logic

---
+++ modules/morbus.certatio/emulation/emulate_samples.py
@@ -0,0 +1,104 @@
+import argparse
+import logging
+
+import speakeasy
+from pefile import PEFormatError
+from unicorn import UcError
+
+import os
+import sys
+import time
+import json
+from numpy import sum
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Collects tracelogs from sample emulation.")
+
+    # data specifics
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--samples", type=str, nargs="+", help="Path to PE file(s) for emulation. Can specify multiple (separate by space).")
+    group.add_argument("--sample-prefix", type=str, nargs="+", help="Prefix of path PE files for emulation. Can specify multiple (separate by space).")
+
+    parser.add_argument("--output", type=str, default="reports", help="Directory where to store emulation reports.")
+
+    parser.add_argument("--logfile", type=str, help="File to store logging messages")
+    parser.add_argument("--debug", action="store_true", help="Provide with DEBUG level information from packages")
+
+    args = parser.parse_args()
+
+    # if logfile argument present - log to a file instead of stdout
+    level = logging.DEBUG if args.debug else logging.WARNING
+    logconfig = {
+        "level": level,
+        "format": "%(asctime)-15s %(name)-5s %(levelname)-8s %(message)s"
+    }
+    if args.logfile:
+        logconfig["handlers"] = [logging.FileHandler(args.logfile, 'a', 'utf-8')]
+    logging.basicConfig(**logconfig)
+    
+    logging.warning(f" [!] Starting emulation.")
+
+    # parsing files
+    filelist = []
+    if args.samples:
+        for file in args.samples:
+            if os.path.exists(file):
+                if os.path.isdir(file):
+                    logging.error(f" [-] {file} is a directory... If you want parse samples within that directory, use --sample-prefix!")
+                    sys.exit(1)
+                filelist.append(file)
+            else:
+                logging.error(f" [-] {file} does not exist...")
+    elif args.sample_prefix:
+        for prefix in args.sample_prefix:
+            prefix = prefix.rstrip("/")
+            if os.path.exists(prefix):
+                # folder
+                filelist.extend([f"{prefix}/{x}" for x in os.listdir(prefix)])
+            else:
+                folder = "/".join(prefix.split("/")[:-1])
+                if os.path.exists(folder):
+                    prefix = prefix.split("/")[-1]
+                    files = [f"{folder}/{x}" for x in os.listdir(folder) if x.startswith(prefix)]
+                    filelist.extend(files)
+                else:
+                    logging.error(f" [-] {folder} folder does not exist...")
+    
+    else:
+        logging.error(" [-] Specify either --samples or --sample-prefix.")
+        sys.exit(1)
+    
+    # emulate samples
+    #timestamp = int(time.time())
+    report_folder = f"{args.output}"
+    os.makedirs(report_folder, exist_ok=True)
+    
+    l = len(filelist)
+    for i,file in enumerate(filelist):    
+        logging.debug(f" [*] {i}/{l}: emulating {file} ...")
+        
+        samplename = file.split("/")[-1]
+        reportfile = f"{report_folder}/{samplename}.json"
+        if os.path.exists(reportfile):
+            logging.warning(f" [!] Exists, skipping analysis: {reportfile}\n")
+            continue
+        try:
+            se = speakeasy.Speakeasy(config=json.load(open("./config.json")))
+            module = se.load_module(file)
+            se.run_module(module)
+            report = se.get_report()
+
+            took = report["emulation_total_runtime"]
+            api_seq_len = sum([len(x["apis"]) for x in report["entry_points"]])
+            logging.debug(f" [+] Finished emulation, took: {took:.2f}s, API calls acquired: {api_seq_len}\n")
+            
+            with open(f"{reportfile}", "w") as f:
+                json.dump(report["entry_points"], f, indent=4)
+
+        except PEFormatError as ex:
+            logging.error(f" [-] Failed emulation, PEFormatError: {file}\n{ex}\n")
+        except UcError as ex:
+            logging.error(f" [-] Failed emulation, UcError: {file}\n{ex}\n")
+        except IndexError as ex:
+            logging.error(f" [-] Failed emulation, IndexError: {file}\n{ex}\n")
