1c2fc2e6c746bc48ad37df3ec49b09ead58e8aeb
Dmitrijs Trizna <dmitrijs.trizna@avast.com>
2022-03-05 13:20:35 +0100

    functional emulation code

--- modules/morbus.certatio/emulation/emulate_samples.py
+++ modules/morbus.certatio/emulation/emulate_samples.py
@@ -7,9 +7,73 @@ from unicorn import UcError
 
 import os
 import sys
-import time
 import json
+import time
+from pathlib import Path
 from numpy import sum
+
+import threading
+
+def write_error(errfile):
+    # creating of empty file 
+    Path(errfile).touch()
+
+
+def emulate(file, i, l):
+    samplename = file.split("/")[-1]
+    reportfile = f"{report_folder}/{samplename}.json"
+    errfile = f"{report_folder}/{samplename}.err"
+
+    if os.path.exists(reportfile):
+        logging.warning(f" [!] {i}/{l} Exists, skipping analysis: {reportfile}\n")
+    elif os.path.exists(errfile):
+        logging.warning(f" [!] {i}/{l} Exists, skipping analysis: {errfile}\n")
+    else:
+        try:
+            se = speakeasy.Speakeasy(config=json.load(open("./speakeasy_config.json")))
+            module = se.load_module(file)
+            se.run_module(module)
+            report = se.get_report()
+
+            took = report["emulation_total_runtime"]
+            api_seq_len = sum([len(x["apis"]) for x in report["entry_points"]])
+            
+            if api_seq_len >= 1:
+                # 1 API call is sometimes already enough
+                with open(f"{reportfile}", "w") as f:
+                    json.dump(report["entry_points"], f, indent=4)
+            if api_seq_len == 1 or api_seq_len == 0:
+                # some uninformative failures with 0 or 1 API calls - e.g. ordinal_100
+                api = [x['apis'] for x in report['entry_points']][0] if api_seq_len ==1 else ''
+                err = [x['error']['type'] if "error" in x.keys() and "type" in x["error"].keys() else "" for x in report['entry_points']]
+                if "unsupported_api" in err:
+                    err.extend([x['error']['api']['name'] for x in report['entry_points']])
+                logging.debug(f" [D] {i}/{l} API nr.: {api_seq_len}; Err: {err}; APIs: {api}")
+                if api_seq_len == 0:
+                    write_error(errfile)
+
+            logging.warning(f" [+] {i}/{l} Finished emulation {file}, took: {took:.2f}s, API calls acquired: {api_seq_len}\n")
+
+        except PEFormatError as ex:
+            logging.error(f" [-] {i}/{l} Failed emulation, PEFormatError: {file}\n{ex}\n")
+            write_error(errfile)
+        except UcError as ex:
+            logging.error(f" [-] {i}/{l} Failed emulation, UcError: {file}\n{ex}\n")
+            write_error(errfile)
+        except IndexError as ex:
+            logging.error(f" [-] {i}/{l} Failed emulation, IndexError: {file}\n{ex}\n")
+            write_error(errfile)
+        except speakeasy.errors.NotSupportedError as ex:
+            logging.error(f" [-] {i}/{l} Failed emulation, NotSupportedError: {file}\n{ex}\n")
+            write_error(errfile)
+        except speakeasy.errors.SpeakeasyError as ex:
+            logging.error(f" [-] {i}/{l} Failed emulation, SpeakEasyError: {file}\n{ex}\n")
+            write_error(errfile)
+        except Exception as ex:
+            logging.error(f" [-] {i}/{l} Failed emulation, general Exception: {file}\n{ex}\n")
+            write_error(errfile)
+        
+
 
 if __name__ == "__main__":
 
@@ -21,6 +85,9 @@ if __name__ == "__main__":
     group.add_argument("--sample-prefix", type=str, nargs="+", help="Prefix of path PE files for emulation. Can specify multiple (separate by space).")
 
     parser.add_argument("--output", type=str, default="reports", help="Directory where to store emulation reports.")
+    
+    parser.add_argument("--start-idx", type=int, default=0, help="If provided, emulation will start from file with this index.")
+    parser.add_argument("--threads", type=int, default=5, help="If provided, emulation will start from file with this index.")
 
     parser.add_argument("--logfile", type=str, help="File to store logging messages")
     parser.add_argument("--debug", action="store_true", help="Provide with DEBUG level information from packages")
@@ -37,7 +104,7 @@ if __name__ == "__main__":
         logconfig["handlers"] = [logging.FileHandler(args.logfile, 'a', 'utf-8')]
     logging.basicConfig(**logconfig)
     
-    logging.warning(f" [!] Starting emulation.")
+    logging.warning(f" [!] Starting emulation.\n")
 
     # parsing files
     filelist = []
@@ -75,47 +142,13 @@ if __name__ == "__main__":
     os.makedirs(report_folder, exist_ok=True)
 
     l = len(filelist)
+    
     for i,file in enumerate(filelist):    
-        logging.debug(f" [*] {i}/{l}: emulating {file} ...")
-        
-        samplename = file.split("/")[-1]
-        reportfile = f"{report_folder}/{samplename}.json"
-        if os.path.exists(reportfile):
-            logging.warning(f" [!] Exists, skipping analysis: {reportfile}\n")
-            continue
-        try:
-            se = speakeasy.Speakeasy(config=json.load(open("./speakeasy_config.json")))
-            module = se.load_module(file)
-            se.run_module(module)
-            report = se.get_report()
+        if i < args.start_idx:
+            continue    
+        while len(threading.enumerate()) > args.threads:
+            time.sleep(0.1)
 
-            took = report["emulation_total_runtime"]
-            api_seq_len = sum([len(x["apis"]) for x in report["entry_points"]])
-            
-            if api_seq_len >= 1:
-                # 1 API call is sometimes already enough
-                with open(f"{reportfile}", "w") as f:
-                    json.dump(report["entry_points"], f, indent=4)
-            if api_seq_len == 1 or api_seq_len == 0:
-                # some uninformative failures with 0 or 1 API calls - e.g. ordinal_100
-                api = [x['apis'] for x in report['entry_points']][0] if api_seq_len ==1 else ''
-                err = [x['error']['type'] if "error" in x.keys() and "type" in x["error"].keys() else "" for x in report['entry_points']]
-                if "unsupported_api" in err:
-                    err.extend([x['error']['api']['name'] for x in report['entry_points']])
-                logging.debug(f" [DBG]: API nr.: {api_seq_len}; Err: {err}; APIs: {api}")
-
-            logging.debug(f" [+] Finished emulation, took: {took:.2f}s, API calls acquired: {api_seq_len}\n")
-
-
-        except PEFormatError as ex:
-            logging.error(f" [-] Failed emulation, PEFormatError: {file}\n{ex}\n")
-        except UcError as ex:
-            logging.error(f" [-] Failed emulation, UcError: {file}\n{ex}\n")
-        except IndexError as ex:
-            logging.error(f" [-] Failed emulation, IndexError: {file}\n{ex}\n")
-        except speakeasy.errors.NotSupportedError as ex:
-            logging.error(f" [-] Failed emulation, NotSupportedError: {file}\n{ex}\n")
-        except speakeasy.errors.SpeakeasyError as ex:
-            logging.error(f" [-] Failed emulation, SpeakEasyError: {file}\n{ex}\n")
-        except Exception as ex:
-            logging.error(f" [-] Failed emulation, general Exception: {file}\n{ex}\n")
+        t = threading.Thread(target=emulate, args=(file, i, l))
+        t.start()
+        logging.debug(f" [D] Started theread: {i}\{l}")
