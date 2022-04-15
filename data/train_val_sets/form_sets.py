e27f40dc9bf5d7ce6eba829d63ef1f9f428bdbd2
Dmitrijs Trizna <dmitrijs.trizna@avast.com>
2022-04-15 11:16:11 +0200

    testset and validation ambiguity

--- data/train_val_sets/form_sets.py
+++ data/train_val_sets/form_sets.py
@@ -24,11 +24,11 @@ y = np.hstack([np.ones(len(X_malicious_raw)),np.zeros(len(X_clean_raw))])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, shuffle=True, random_state=1763)
 
-with open("X_train.pickle", "wb") as f:
+with open("X_train.pickle.set", "wb") as f:
     pickle.dump(X_train, f)
 
-with open("X_test.pickle", "wb") as f:
+with open("X_val.pickle.set", "wb") as f:
     pickle.dump(X_test, f)
 
 np.save("y_train", y_train)
-np.save("y_test", y_test)
+np.save("y_val", y_test)
