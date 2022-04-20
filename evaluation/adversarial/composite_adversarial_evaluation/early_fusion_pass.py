f7eed4a81befdf9b4cdbbc3df32221a1750298b0
Dmitrijs Trizna <dmitrijs.trizna@avast.com>
2022-04-20 14:36:31 +0200

    finalizing adversarial robustness statistics

--- evaluation/adversarial/composite_adversarial_evaluation/early_fusion_pass.py
+++ evaluation/adversarial/composite_adversarial_evaluation/early_fusion_pass.py
@@ -13,6 +13,12 @@ repo_root = "/data/quo.vadis/"
 sys.path.append(repo_root)
 from models import CompositeClassifier
 
+def get_adversarial_samples(folder):
+    fullpaths = [repo_root+folder+x for x in os.listdir(repo_root+folder)]
+    adversarial_samples = [x for x in fullpaths if not os.path.islink(x)]
+    adversarial_samples.sort()
+    return adversarial_samples
+
 # ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_15sections_10population/"
 #ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_10sections_10population/"
 ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_5sections_10population/"
@@ -21,33 +27,43 @@ ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_5sect
 #ADVERSARIAL_RAW_SET_FOLDER = "data/adversarial.samples/samples_adversarial_testset_gamma_ember_sections/10/"
 ADVERSARIAL_RAW_SET_FOLDER = "data/adversarial.samples/samples_adversarial_testset_gamma_ember_sections/5/"
 
+ADV_SAMPLES = get_adversarial_samples(ADVERSARIAL_RAW_SET_FOLDER)
+ADV_SAMPLE_HASHES = [x.split("/")[-1] for x in ADV_SAMPLES]
+
 #ARRAY_FOLDER  = "evaluation/adversarial/composite_adversarial_evaluation/arrays_ember_15sections_10population/"
 #ARRAY_FOLDER  = repo_root + "evaluation/adversarial/composite_adversarial_evaluation/arrays_ember_10sections_10population/"
 ARRAY_FOLDER  = repo_root + "evaluation/adversarial/composite_adversarial_evaluation/arrays_ember_5sections_10population/"
 os.makedirs(ARRAY_FOLDER, exist_ok=True)
 
+# ====================================
 # STATISTICS
 adversarial_emulated_files = os.listdir(repo_root + ADVERSARIAL_EMULATED_SET_FOLDER)
-adversarial_reports = [x.rstrip(".json") for x in adversarial_emulated_files if x.endswith(".json")]
-print("Successful adversarial emulation reports: ", len(adversarial_reports))
+adversarial_reports = [x.replace(".json","") for x in adversarial_emulated_files if x.endswith(".json")]
+ADV_REPORTS = [x for x in adversarial_reports if x in ADV_SAMPLE_HASHES]
 
-adversarial_errors = [x for x in adversarial_emulated_files if x.endswith(".err")]
-print("Errored adversarial emulation reports: ", len(adversarial_errors))
+adv_only_reports = len(ADV_REPORTS)
+print("Successful adversarial emulation reports: ", adv_only_reports)
 
-print("Total adversarial samples: ", len(adversarial_errors) + len(adversarial_reports))
-print(f"Emulation success rate: {len(adversarial_reports)/(len(adversarial_reports)+len(adversarial_errors))*100:.2f}%")
+adversarial_errors = len(ADV_SAMPLE_HASHES) - adv_only_reports
+print("Errored adversarial emulation reports: ", adversarial_errors)
 
+print("Total adversarial samples: ", adversarial_errors + adv_only_reports)
+print(f"Emulation success rate: {adv_only_reports/(adv_only_reports+adversarial_errors)*100:.2f}%")
+
+# ====================================
 # EARLY FUSION PASSES
 # 1. ADVERSARIAL SAMPLES
-classifier = CompositeClassifier(late_fusion_model="LogisticRegression", root=repo_root,
+classifier = CompositeClassifier(root=repo_root, 
                                 emulation_report_path=ADVERSARIAL_EMULATED_SET_FOLDER,
                                 rawpe_db_path=ADVERSARIAL_RAW_SET_FOLDER)
 # preprocess samples using correct report path
-x_adv_ember = classifier.preprocess_pelist(adversarial_reports, dump_xy=True) # takes ~30m
-np.save(ARRAY_FOLDER + "X-gamma-vs-ember-early-fusion-pass.arr", x_adv_ember)
+x_adv_ember = classifier.preprocess_pelist(ADV_REPORTS, dump_xy=True) # takes ~30m
+np.save(ARRAY_FOLDER + "X-gamma-vs-ember-early-fusion-pass-only-adv.arr", x_adv_ember)
 
 # 2. ORIGINAL SAMPLES - TO HAVE THE SAME HASHES (CANNOT BE ACQUIRES FROM EXISTING ARRAYS)
-classifier = CompositeClassifier(late_fusion_model="LogisticRegression", root=repo_root)
+classifier_orig = CompositeClassifier(root=repo_root)
 # preprocess samples using correct report path
-x_orig_ember = classifier.preprocess_pelist(adversarial_reports, dump_xy=True) # takes ~30m
-np.save(ARRAY_FOLDER + "X-gamma-vs-ember-early-fusion-pass-orig.arr", x_orig_ember)
+x_orig_ember = classifier_orig.preprocess_pelist(ADV_REPORTS, dump_xy=True) # takes ~30m
+np.save(ARRAY_FOLDER + "X-gamma-vs-ember-early-fusion-pass-orig-only-adv.arr", x_orig_ember)
+
+import pdb;pdb.set_trace()
--- evaluation/adversarial/composite_adversarial_evaluation/get_secml_ember_scores.py
+++ evaluation/adversarial/composite_adversarial_evaluation/get_secml_ember_scores.py
@@ -10,6 +10,12 @@ from secml.array import CArray
 from secml_malware.models.c_classifier_ember import CClassifierEmber
 from secml_malware.attack.blackbox.c_wrapper_phi import CEmberWrapperPhi
 
+def get_adversarial_samples(folder):
+    fullpaths = [repo_root+folder+x for x in os.listdir(repo_root+folder)]
+    adversarial_samples = [x for x in fullpaths if not os.path.islink(x)]
+    adversarial_samples.sort()
+    return adversarial_samples
+
 # ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_15sections_10population/"
 #ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_10sections_10population/"
 ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_5sections_10population/"
@@ -18,13 +24,16 @@ ADVERSARIAL_EMULATED_SET_FOLDER = "data/adversarial.emulated/reports_ember_5sect
 #ADVERSARIAL_RAW_SET_FOLDER = "data/adversarial.samples/samples_adversarial_testset_gamma_ember_sections/10/"
 ADVERSARIAL_RAW_SET_FOLDER = "data/adversarial.samples/samples_adversarial_testset_gamma_ember_sections/5/"
 
+ADV_SAMPLES = get_adversarial_samples(ADVERSARIAL_RAW_SET_FOLDER)
+ADV_SAMPLE_HASHES = [x.split("/")[-1] for x in ADV_SAMPLES]
+
 #ARRAY_FOLDER  = "evaluation/adversarial/composite_adversarial_evaluation/arrays_ember_15sections_10population/"
 #ARRAY_FOLDER  = repo_root + "evaluation/adversarial/composite_adversarial_evaluation/arrays_ember_10sections_10population/"
 ARRAY_FOLDER  = repo_root + "evaluation/adversarial/composite_adversarial_evaluation/arrays_ember_5sections_10population/"
 
-
 adversarial_emulated_files = os.listdir(repo_root + ADVERSARIAL_EMULATED_SET_FOLDER)
-adversarial_reports = [x.rstrip(".json") for x in adversarial_emulated_files if x.endswith(".json")]
+adversarial_reports = [x.replace(".json","") for x in adversarial_emulated_files if x.endswith(".json")]
+ADV_REPORTS = [x for x in adversarial_reports if x in ADV_SAMPLE_HASHES]
 
 db_orig = rawpe_db()
 db_ember = rawpe_db(PE_DB_PATH=repo_root + ADVERSARIAL_RAW_SET_FOLDER)
@@ -41,17 +50,17 @@ def get_y(net, path):
     y = confidence[1][0].item()
     return y
 
-y_malconv_orig = []
-y_malconv_adv = []
-l = len(adversarial_reports)
-for i,h in enumerate(adversarial_reports):
+y_ember_orig = []
+y_ember_adv = []
+l = len(ADV_REPORTS)
+for i,h in enumerate(ADV_REPORTS):
     print(f"{i}/{l}", end="\r")
     # orig:
     path = db_orig[h]
-    y_malconv_orig.append(get_y(net, path))
+    y_ember_orig.append(get_y(net, path))
     # adv
     path = db_ember[h]
-    y_malconv_adv.append(get_y(net, path))
+    y_ember_adv.append(get_y(net, path))
 
-np.save(ARRAY_FOLDER + "y-gamma-vs-ember-scores-orig.npy", np.stack(y_malconv_orig))
-np.save(ARRAY_FOLDER + "y-gamma-vs-ember-scores.npy", np.stack(y_malconv_adv))
+np.save(ARRAY_FOLDER + "y-gamma-vs-ember-scores-orig-only-adv.npy", np.stack(y_ember_orig))
+np.save(ARRAY_FOLDER + "y-gamma-vs-ember-scores-only-adv.npy", np.stack(y_ember_adv))
